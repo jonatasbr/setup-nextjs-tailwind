0ce57f040725bb1dbf459854d9cb25d4af33c457
Jonatas Sousa <jonatasbraz@gmail.com>
2022-02-09 17:01:29 -0300

    added plop generate and improved prettier`s config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@ module.exports = {
   mode: 'jit',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}'
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
